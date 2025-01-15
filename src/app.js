8f6c6c0dc82834550011a8eb7cacea54f3e1da22
mirzabakti <mirza.bakti@gmail.com>
2025-01-15 14:15:45 +0700

    validasi CRUD users

--- src/app.js
+++ src/app.js
@@ -16,11 +16,11 @@ const app = express();
 app.use(express.json());
 
 // Import rute API
-// const userRoutes = require('./routes/userRoutes');
+const userRoutes = require('./routes/userRoutes');
 // const financeRoutes = require('./routes/financeRoutes');
 
 // Gunakan rute API
-// app.use('/api/users', userRoutes);
+app.use('/api/users', userRoutes);
 // app.use('/api/finance', financeRoutes);
 
 // Port aplikasi
